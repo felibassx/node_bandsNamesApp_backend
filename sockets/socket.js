a3ada3709155743bcdeb460a75773a185f979b00
kafecode <kafecode@gmail.com>
2020-10-11 23:29:51 -0300

    jj

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,67 +1,54 @@
+const { io } = require('../index');
+
 const Bands = require('../models/bands');
 const Band = require('../models/band');
 
-const {io} = require('../index');
 const bands = new Bands();
 
-console.log('init server ...');
+bands.addBand( new Band( 'Breaking Benjamin' ) );
+bands.addBand( new Band( 'Bon Jovi' ) );
+bands.addBand( new Band( 'Héroes del Silencio' ) );
+bands.addBand( new Band( 'Metallica' ) );
 
-// añadir bandas
-bands.addBand( new Band('Queen') );
-bands.addBand( new Band('Black Sabat') );
-bands.addBand( new Band('Red Hot Chilli Peppers') );
-bands.addBand( new Band('Los Tres') );
-bands.addBand( new Band('Radio Head') );
-bands.addBand( new Band('Dream theater') );
 
-console.log(bands);
-
-// Mensajes de sockets
+// Mensajes de Sockets
 io.on('connection', client => {
-
-    console.log('cliente conectado');
+    console.log('Cliente conectado');
 
     client.emit('active-bands', bands.getBands() );
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje', ( payload ) => {
+        console.log('Mensaje', payload);
+        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+    });
 
     client.on('vote-band', (payload) => {
 
         bands.voteBand( payload.id );
-        io.emit('active-bands', bands.getBands());
-
+        io.emit('active-bands', bands.getBands() );
     });
 
     client.on('add-band', (payload) => {
-
-        const newBand = new Band(payload.name);
+        const newBand = new Band( payload.name );
         bands.addBand( newBand );
-        io.emit('active-bands', bands.getBands());
-
+        io.emit('active-bands', bands.getBands() );
     });
 
     client.on('delete-band', (payload) => {
 
-        bands.deleteBands( payload.id );
-        io.emit('active-bands', bands.getBands());
-
+        bands.deleteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
     });
 
-    client.on('disconnect', () => { console.log('cliente desconectado'); });
+    // client.on('emitir-mensaje', ( payload ) => {
+    //     // console.log(payload);
+    //     // io.emit('nuevo-mensaje', payload ); // emite a todos!
+    //     client.broadcast.emit('nuevo-mensaje', payload ); // emite a todos menos el que lo emitió
+    // })
 
-    client.on('mensaje', (payload) => {
-        console.log('Mensaje!!', payload);
-        io.emit('mensaje', {admin: 'Nuevo mensaje'});
-    });
-
-    client.on('emitir-mensaje', (payload) => {
-
-        var n = new Bands
-
-        console.log(payload);
-        // emite  el mensaje  a todos los clientes conectados
-        // io.emit('nuevo-mensaje', payload);
-
-        // emite a todos los clientes conectados menos al quien lo emite
-        client.broadcast.emit('nuevo-mensaje', payload);
-    });
 
 });
